7cb0058e9b3f6a6fea1f693c3938c87666c458bd
Asad Rizvi <=>
2025-04-10 22:46:59 +0300

    updated login and registration screen UI

--- frontend/lib/utils/config.dart
+++ frontend/lib/utils/config.dart
@@ -1,1 +1,6 @@
-const String BASE_URL = "https://roomio.fly.dev";
+// When running in Android Emulator connecting to a VM backend
+// const String BASE_URL = "http://10.0.2.2:8000";
+// const String BASE_URL = "https://roomio.fly.dev";
+// Other possible configurations:
+const String BASE_URL = "http://127.0.0.1:8000";  // For web or desktop testing
+// const String BASE_URL = "http://localhost:8000";   // For iOS Simulator
