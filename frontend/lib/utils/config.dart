ddec64af15e9bf6f0f7c63ca7fa576f0fbf89280
Asad Rizvi <=>
2025-04-11 20:10:14 +0300

    added all urls to config.dart for testing

--- frontend/lib/utils/config.dart
+++ frontend/lib/utils/config.dart
@@ -1,6 +1,6 @@
 // When running in Android Emulator connecting to a VM backend
 // const String BASE_URL = "http://10.0.2.2:8000";
-// const String BASE_URL = "https://roomio.fly.dev";
+const String BASE_URL = "https://roomio.fly.dev";  // Production URL
 // Other possible configurations:
-const String BASE_URL = "http://127.0.0.1:8000";  // For web or desktop testing
+// const String BASE_URL = "http://127.0.0.1:8000";  // For web or desktop testing
 // const String BASE_URL = "http://localhost:8000";   // For iOS Simulator
